ca587e9ac69acaed11ab38044e948d070041c34f
May Jawdat <may.jawdat@gmail.com>
2017-08-31 16:24:44 -0700

    Add delete method

--- simpleblog/app/controllers/posts_controller.rb
+++ simpleblog/app/controllers/posts_controller.rb
@@ -27,13 +27,22 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    
+
     if (@post.update(post_params))
       redirect_to @post
     else
       render 'edit'
     end
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
 
   private def post_params
     params.require(:post).permit(:title, :body)
